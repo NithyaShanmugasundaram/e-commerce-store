fffe645258ebd34d6fdcaa71dae88eaa72472065
Shanmugasundaram <nithya.a.shanmugasundaram@capgemini.com>
2025-02-27 22:55:06 +0800

    auth credentials added

--- db/seed.ts
+++ db/seed.ts
@@ -4,6 +4,11 @@ import sampleData from './sample-data';
 async function main() {
   const prisma = new PrismaClient();
   await prisma.product.deleteMany();
+  await prisma.user.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  
 
   await prisma.product.createMany({ 
     data: sampleData.products.map(product => ({
@@ -11,7 +16,10 @@ async function main() {
       isFeatured: product.isFeatured ?? false,
       banner: product.banner ?? ''
     }))
+  
+ 
   });
+  await prisma.user.createMany({data: sampleData.users});
 
   console.log('Database seeded successfully!');
 }
